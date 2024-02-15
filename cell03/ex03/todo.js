565320d7a8bfe4bf1f6c541cd832445340d846dd
Benjamin <poisson.benjamin69@gmail.com>
2024-02-15 11:51:24 +0100

    Fin 3.3

---
+++ cell03/ex03/todo.js
@@ -0,0 +1,36 @@
+let list = [];
+
+function addTask (task_name) {
+    var new_task = document.createElement('div');
+    new_task.textContent = task_name;
+    new_task.id = task_name;
+    new_task.className = 'todo';
+    new_task.onclick = function() { removeTask(task_name)};
+    document.getElementById('ft_list').prepend(new_task);
+    list.push(task_name);
+    saveCookie();
+}
+
+function removeTask (task_name) {
+    if (window.confirm("Are you sure to delete this task ?")) {
+        document.getElementById(task_name).remove();
+        list.splice(list.indexOf(task_name),1);
+        saveCookie();
+    }
+
+}
+
+function enterName () {
+    var task_name = window.prompt("Enter task name",'name');
+    if (task_name == "" ) {
+        console.log('No task name');
+    } else {
+        addTask(task_name);
+    }
+
+}
+
+function saveCookie () {
+    document.cookie = list;
+    console.log(list);
+}
